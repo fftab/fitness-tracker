065359332239550a591bded0b9ad4787d42bcd2e
Florencia Tabarez <fftabarez97@gmail.com>
2020-07-23 22:09:18 -0600

    Updated PORT.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
